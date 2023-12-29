cc9245b3d1517cd8647e4f21bdf8c0bebec8c683
Brijesh Prajapati <108275306+brijeshprajapati98@users.noreply.github.com>
2023-12-29 13:43:08 +0530

    Stack and Heap Memory

--- .vscode/01_basics/Datatypes-Summary.js
+++ .vscode/01_basics/Datatypes-Summary.js
@@ -36,3 +36,32 @@ const myFuction = function(){
 }
 
 console.log(typeof bignumber);
+
+
+/*************************** Stack and Heap memory **********************************/
+
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+
+let myYoutubename = "helloYoutube"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+
+}
+
+let userTwo = userOne
+
+userTwo.email = "brijesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
